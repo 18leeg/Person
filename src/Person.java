c5fe43dc32ae2ede61a281fc0f4dcf437f41bf53
Gene Lee <leexgenex@gmail.com>
2018-03-04 11:09:31 -0600

    Initial Class Definition

---
+++ src/Person.java
@@ -0,0 +1,14 @@
+
+public class Person {
+	private String name;
+	private double salary;
+	
+	public Person(){
+		name = "Whomst";
+		salary = 5000.00;
+	}
+	
+	public void moreMoney(){
+		salary+=1000.0;
+	}
+}
